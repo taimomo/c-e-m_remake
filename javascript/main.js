419bf01cb95c67cf5a4d4fcea0f26c256788eb46
Taizo Shimono <t_shimomo1407@yahoo.co.jp>
2020-04-29 22:25:23 +0900

    header and footer separate in index.html and jQuery setting

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+// function header(rootDir) {
+//   $.ajax({
+//     url: rootDir + "include/header.html",
+//     cache: false,
+//     async: false,
+//     dataType: "html",
+//     success: function (html) {
+//       html = html.replace(/\{\$root\}/g, rootDir);
+//       document.write(html);
+//     }
+//   });
+// }
