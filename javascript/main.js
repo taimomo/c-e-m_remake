971232de45e46c0b71ad31af772fa67bc7ee037b
Taizo Shimono <t_shimomo1407@yahoo.co.jp>
2020-10-27 18:34:27 +0900

    desktop index page create

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,18 @@
-"use strict";
-{
-  // headerとfooterの表示
-  $(function () {
-    // $("header").load("./include/header.html");
-    $("footer").load("./include/footer.html");
-  });
+// headerとfooterの表示
+$(function () {
+  "use strict";
+  // $("header").load("./include/header.html");
+  $("footer").load("./include/footer.html");
+});
 
-  // 隠れているメニュー表示
-  $(document).ready(function () {
-    $(document).on("click", ".el_nav-text", function () {
-      if ($(".bl_list-dropdown", this).css("display") == "none") {
-        $(".bl_list-dropdown", this).slideDown();
-      } else {
-        $(".bl_list-dropdown", this).slideUp();
-      }
-    });
+// 隠れているメニュー表示
+$(document).ready(function () {
+  "use strict";
+  $(document).on("click", ".el_nav-text", function () {
+    if ($(".bl_list-dropdown", this).css("display") == "none") {
+      $(".bl_list-dropdown", this).slideDown();
+    } else {
+      $(".bl_list-dropdown", this).slideUp();
+    }
   });
-}
+});
