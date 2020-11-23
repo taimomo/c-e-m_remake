c1ec83e4657c757b6204956569365f62527f5cae
Taizo Shimono <t_shimomo1407@yahoo.co.jp>
2020-11-23 14:04:32 +0900

    mobile menu create and jquery setting fixed

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,19 @@
 // headerとfooterの表示
 $(function () {
   "use strict";
-  // $("header").load("./include/header.html");
+  $("header").load("./include/header.html");
+  $(".overlay").load("./include/overlay.html");
   $("footer").load("./include/footer.html");
 });
 
 // 隠れているメニュー表示
-$(document).ready(function () {
+$(document).on("click", ".el_nav-text", function () {
   "use strict";
-  $(document).on("click", ".el_nav-text", function () {
-    if ($(".bl_list-dropdown", this).css("display") == "none") {
-      $(".bl_list-dropdown", this).slideDown();
-    } else {
-      $(".bl_list-dropdown", this).slideUp();
-    }
-  });
+  if ($(".bl_list-dropdown", this).css("display") == "none") {
+    $(".bl_list-dropdown", this).slideDown();
+  } else {
+    $(".bl_list-dropdown", this).slideUp();
+  }
 });
 
 $(function () {
@@ -40,3 +39,37 @@ $(function () {
     return false;
   });
 });
+
+$(function () {
+  "use strict";
+  $(document).on("click", ".el_btn-nav", function () {
+    $(".bl_overlay").fadeToggle();
+    $(".el_btn-hm i").toggleClass("fa-bars").toggleClass("fa-times");
+  });
+});
+
+$(function () {
+  "use strict";
+
+  $(document).on("click", ".bl_over-navItem", function () {
+    if ($(".bl_over-acc", this).css("display") == "none") {
+      $(".bl_over-acc", this).slideDown();
+      $(".bl_over-navTxt .el_icon-toggle", this)
+        .toggleClass("fa-plus")
+        .toggleClass("fa-minus");
+    } else {
+      $(".bl_over-acc", this).slideUp();
+      $(".bl_over-navTxt .el_icon-toggle", this)
+        .toggleClass("fa-plus")
+        .toggleClass("fa-minus");
+    }
+  });
+});
+
+// $(function () {
+//   "use strict";
+//   $(document).on("click", ".bl_overlay", function () {
+//     $(".bl_overlay").fadeToggle(200);
+//     $(".el_btn-hm i").toggleClass("fa-bars").toggleClass("fa-times");
+//   });
+// });
